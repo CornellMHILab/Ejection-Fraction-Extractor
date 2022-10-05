45e5e2f9a86d01c95295d9c0ba153a285c9fbb71
pra2008 <pra2008@med.cornell.edu>
2022-10-05 10:00:17 -0400

    Update DatabaseListenerConfig.groovy

--- target/classes/listeners/DatabaseListenerConfig.groovy
+++ target/classes/listeners/DatabaseListenerConfig.groovy
@@ -6,20 +6,10 @@ int batchSize = 1000
 
 // String url = "jdbc:mysql://localhost:3306/clarityhf?autoReconnect=true&useSSL=false"
 // String driver = "com.mysql.jdbc.Driver"
-// String dbUser = "clarityhf"
-// String dbPwd = "clarityhf"
-// String dbsName = "clarityhf"
-// String tableName = "lvef_output_test"
-// incomingTypes = "gov.va.vinci.ef.types.Relation"
-// // incomingTypes = "gov.va.vinci.ef.types.MeasurementWindow"
-
-
-String url = "jdbc:sqlserver://vits-archsqlp03.a.wcmc-ad.net:1433;databasename=SUPER_NLP;integratedSecurity=false"
-String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-String dbUser = "usr_dm_nlp"
-String dbPwd = "us3r_83_Nlp"
-String dbsName = "SUPER_NLP"
-String tableName = "dbo.EFEX_LEO_OUTPUT"
+// String dbUser = "<usr>"
+// String dbPwd = "<pwd>"
+// String dbsName = "<db>"
+// String tableName = "<table>"
 incomingTypes = "gov.va.vinci.ef.types.Relation"
 
 fieldList = [
